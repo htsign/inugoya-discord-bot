46daea1f70775c5d8c18ebc019d12ac06b956676
htsign <htsign@outlook.com>
2022-11-19 16:32:43 +0900

    implement basic logic

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const keywords = require('./keywords.json');
+
+const template = ` 彡⌒ミ
+(´･ω･\`)　また髪の話してる・・・
+(|　　 |)::::`;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+client.once(Events.ClientReady, () => {
+  console.log('watches...', keywords);
+});
+client.on(Events.MessageCreate, ({ content, author, channel }) => {
+  if (author.bot) return;
+  
+  console.log('incoming: ', content);
+  if (keywords.some(keyword => content.includes(keyword))) {
+    channel.send(template);
+  }
+});
+
+const token = process.env.ACCESS_TOKEN;
+client.login(token);
