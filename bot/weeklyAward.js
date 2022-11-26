58ece7fcba94817ea82ba769c6951fe1ab2c92d2
htsign <htsign@outlook.com>
2022-11-27 00:37:59 +0900

    weekly award is now post as a message instead of a reply

--- bot/weeklyAward.js
+++ bot/weeklyAward.js
@@ -50,26 +50,28 @@ const tick = async () => {
   const now = dayjs().tz();
 
   if (now.day() === SUNDAY && now.hour() === 0 && now.minute() === 0) {
-    if ([...messages.values()].some(count => count > 0)) {
-      // sort descending order by reactions count
-      const messagesArray = [...messages].sort(([, a], [, b]) => b - a);
-      
-      // take 3 elements
-      for (let i = 0, len = Math.min(messagesArray.length, 3); i < len; ++i) {
-        const [message, count] = messagesArray[i];
-        
-        const rankText = i === 0 ? '最も' : ` ${i + 1}番目に`;
-        message.reply(`【リアクション大賞】
-先週${rankText}リアクションが多かった投稿です！！ [${count}個]`);
+    const guilds = await client.guilds.fetch();
+    const server = await guilds.find(guild => guild.name === 'inugoya')?.fetch();
+    const channels = server?.channels?.cache;
+    const rootChannel = channels?.find(channel => channel.name === 'root');
+
+    if (rootChannel?.type === ChannelType.GuildText) {
+      if ([...messages.values()].some(count => count > 0)) {
+        // sort descending order by reactions count
+        const messagesArray = [...messages].sort(([, a], [, b]) => b - a);
+
+        // take 3 elements
+        for (let i = 0, len = Math.min(messagesArray.length, 3); i < len; ++i) {
+          const [message, count] = messagesArray[i];
+          
+          const rankText = i === 0 ? '最も' : ` ${i + 1}番目に`;
+          await rootChannel.send(`【リアクション大賞】
+先週${rankText}リアクションが多かった投稿です！！ [${count}個]
+${message.url}`);
+        }
       }
-    }
-    else {
-      const guilds = await client.guilds.fetch();
-      const server = await guilds.find(guild => guild.name === 'inugoya')?.fetch();
-      const channels = server?.channels?.cache;
-      const botChannel = channels?.find(channel => channel.name === 'bot');
-      if (botChannel?.type === ChannelType.GuildText) {
-        botChannel?.send(`【リアクション大賞】
+      else {
+        await rootChannel.send(`【リアクション大賞】
 先週はリアクションが付いた投稿はありませんでした！！`);
       }
     }
