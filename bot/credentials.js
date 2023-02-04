d0b8ca4bb33b7ee7891d6770fd8da88f50dc5223
htsign <htsign@outlook.com>
2023-02-04 19:21:35 +0900

    minor refactor

--- bot/credentials.js
+++ bot/credentials.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-const token = dotenv.config().parsed?.ACCESS_TOKEN ?? process.env.ACCESS_TOKEN;
+const token = (dotenv.config().parsed ?? process.env).ACCESS_TOKEN;
 if (token == null) {
   throw new Error('token is empty');
 }
