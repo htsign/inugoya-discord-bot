63f98797883edf95ed82ae5516f5248c2e11037a
htsign <htsign@outlook.com>
2023-02-18 01:46:08 +0900

    rename for fix typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('./bot/features/commands');
 require('./bot/features/hage');
 require('./bot/features/weeklyAwards');
-require('./bot/features/vcAttension');
+require('./bot/features/vcAttention');
 require('./bot/features/shortenUrl');
 require('./bot/features/wikipediaTitle');
 require('./bot/features/discordUrlExpand');
