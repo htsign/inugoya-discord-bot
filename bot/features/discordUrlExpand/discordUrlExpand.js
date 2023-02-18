c856c561e811092f73f7f3bfac15d21b533dde13
htsign <htsign@outlook.com>
2023-02-18 18:01:09 +0900

    cut out the function that message to discord embeds

--- bot/features/discordUrlExpand/discordUrlExpand.js
+++ bot/features/discordUrlExpand/discordUrlExpand.js
@@ -1,64 +1,24 @@
-const { Events, Colors, AttachmentBuilder, EmbedBuilder } = require("discord.js");
+const { Events } = require("discord.js");
 const client = require("../../client");
 const { log } = require("../../lib/log");
+const { messageToEmbeds } = require("../trans");
 
 client.on(Events.MessageCreate, async message => {
   if (message.author.bot) return;
 
   const regExpIterator = message.content.matchAll(/https:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)\b/g) ?? [];
 
-  /** @type {APIEmbed[]} */
-  const embeds = [];
-
-  for (const [url, guildId, channelId, messageId] of regExpIterator) {
+  for (const [, guildId, channelId, messageId] of regExpIterator) {
     try {
       const guild = await client.guilds.fetch(guildId);
       const channel = await guild.channels.fetch(channelId);
 
       if (channel?.isTextBased()) {
         const referredMessage = await channel.messages.fetch(messageId);
+        const embeds = await messageToEmbeds(referredMessage);
 
-        const author = await referredMessage.author.fetch();
-
-        const embed = new EmbedBuilder()
-          .setURL(url)
-          .setAuthor({ name: author.username, url, iconURL: author.displayAvatarURL() })
-          .setTimestamp(referredMessage.editedTimestamp ?? referredMessage.createdTimestamp)
-          .setColor(referredMessage.author.accentColor ?? Colors.Default);
-
-        if (referredMessage.content !== '') {
-          embed.setDescription(referredMessage.content);
-        }
-
-        /** @type {APIEmbedField[]} */
-        const fields = [];
-
-        {
-          const reactions = referredMessage.reactions.cache;
-          const reactionsCount = reactions.reduce((acc, x) => acc + x.count, 0);
-
-          if (reactionsCount > 0) {
-            fields.push({ name: 'Reactions', value: String(reactionsCount) });
-          }
-        }
-        embed.setFields(...fields);
-
-        {
-          const text = referredMessage.channel.name;
-          const iconURL = referredMessage.guild.iconURL();
-
-          if (iconURL != null) {
-            embed.setFooter({ text, iconURL });
-          }
-          else {
-            embed.setFooter({ text });
-          }
-        }
-
-        embeds.push(embed.toJSON());
-
-        for (const attachment of referredMessage.attachments.values()) {
-          embeds.push({ url, image: { url: attachment.url } });
+        if (embeds.length > 0) {
+          await message.channel.send({ embeds });
         }
       }
     }
@@ -70,9 +30,5 @@ client.on(Events.MessageCreate, async message => {
         throw e;
       }
     }
-  }
-
-  if (embeds.length > 0) {
-    await message.channel.send({ embeds });
   }
 });
---
+++ bot/features/trans.js
@@ -0,0 +1,70 @@
+const { EmbedBuilder, Colors } = require("discord.js");
+
+/**
+ * @param {Message<boolean>} message
+ * @returns {Promise<APIEmbed[]>}
+ */
+const messageToEmbeds = async message => {
+  const { channel } = message;
+
+  if (channel.isTextBased() && !channel.isDMBased()) {
+
+    /** @type {APIEmbed[]} */
+    const embeds = [];
+
+    const author = await message.author?.fetch();
+
+    const embed = new EmbedBuilder()
+      .setURL(message.url)
+      .setTimestamp(message.editedTimestamp ?? message.createdTimestamp)
+      .setColor(message.author?.accentColor ?? Colors.Default);
+
+    if (author != null) {
+      embed.setAuthor({ name: author.username, url: message.url, iconURL: author.displayAvatarURL() });
+    }
+
+    if (message.content !== '') {
+      embed.setDescription(message.content);
+    }
+
+    /** @type {APIEmbedField[]} */
+    const fields = [];
+
+    {
+      const reactions = message.reactions.cache;
+      const reactionsCount = reactions.reduce((acc, x) => acc + x.count, 0);
+
+      if (reactionsCount > 0) {
+        fields.push({ name: 'Reactions', value: String(reactionsCount) });
+      }
+    }
+    embed.setFields(...fields);
+
+    {
+      const text = channel.name;
+      const iconURL = message.guild?.iconURL();
+
+      if (iconURL != null) {
+        embed.setFooter({ text, iconURL });
+      }
+      else {
+        embed.setFooter({ text });
+      }
+    }
+
+    embeds.push(embed.toJSON());
+
+    for (const attachment of message.attachments.values()) {
+      embeds.push({ url: message.url, image: { url: attachment.url } });
+    }
+
+    return embeds;
+  }
+  else {
+    return [];
+  }
+};
+
+module.exports = {
+  messageToEmbeds,
+};
