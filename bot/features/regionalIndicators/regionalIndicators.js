934185e9db8f15d599669777b63f302d4de3a70e
htsign <htsign@outlook.com>
2023-02-19 01:33:06 +0900

    `/emojify` accepts numbers

--- bot/features/regionalIndicators/regionalIndicators.js
+++ bot/features/regionalIndicators/regionalIndicators.js
@@ -3,6 +3,15 @@ const codeLowerA = 'a'.codePointAt(0) ?? 0;
 const codeLowerZ = 'z'.codePointAt(0) ?? 0;
 const cpDiff = codeRegionalIndicatorA - codeLowerA;
 
+const code0 = '0'.codePointAt(0) ?? 0;
+const code9 = '9'.codePointAt(0) ?? 0;
+const numberEmojis = Array.from({ length: 10 }, (_, i) => `${i}\u20e3`);
+
+/** @type {(codePoint: number) => boolean} */
+const isLowerAlphabet = cp => codeLowerA <= cp && cp <= codeLowerZ;
+/** @type {(codePoint: number) => boolean} */
+const isNumber = cp => code0 <= cp && cp <= code9;
+
 /**
  * @param {string} text
  * @returns {RegionalIndicatorsResult}
@@ -14,11 +23,21 @@ const toEmojis = text => {
 
   const codePoints = Array.from(text.toLowerCase(), c => c.codePointAt(0) ?? 0);
 
-  if (codePoints.some(cp => cp < codeLowerA || codeLowerZ < cp)) {
-    return { success: false, message: 'アルファベット以外が含まれています。' };
+  if (codePoints.some(cp => !(isLowerAlphabet(cp) || isNumber(cp)))) {
+    return { success: false, message: 'アルファベットまたは算用数字以外が含まれています。' };
   }
 
-  return { success: true, values: codePoints.map(cp => String.fromCodePoint(cpDiff + cp)) };
+  /** @type {(codePoint: number) => string} */
+  const toEmoji = cp => {
+    if (isLowerAlphabet(cp)) {
+      return String.fromCodePoint(cpDiff + cp);
+    }
+    else if (isNumber(cp)) {
+      return numberEmojis[cp - code0];
+    }
+    throw new Error('invalid procedure');
+  };
+  return { success: true, values: codePoints.map(toEmoji) };
 };
 
 module.exports = {
