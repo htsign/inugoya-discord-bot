1f54aa60b5981f7eca388f15a65e6bbf70a96a32
htsign <htsign@outlook.com>
2023-02-24 17:06:29 +0900

    remove no longer needed packages

--- bot/lib/util.ts
+++ bot/lib/util.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
-
 export const URL_REGEX_GLOBAL = /\bhttps?:\/\/\S+/g;
-const configOutput = dotenv.config();
 
 export const getEnv = (key: string, name: string = 'token'): string => {
-  const token = configOutput.parsed?.[key] ?? process.env[key];
+  const token = process.env[key];
   if (token == null) {
     throw new Error(`${name} is empty`);
   }
