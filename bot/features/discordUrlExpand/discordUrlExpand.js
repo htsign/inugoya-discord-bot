03d01a78fc2d84888fbfe3755fb06bb86606c37f
htsign <htsign@outlook.com>
2023-04-04 22:14:27 +0900

    retry fetching messages if failed

--- bot/features/discordUrlExpand/discordUrlExpand.js
+++ bot/features/discordUrlExpand/discordUrlExpand.js
@@ -1,6 +1,9 @@
 const { Events } = require("discord.js");
 const client = require("../../client");
+const { log } = require("../../lib/log");
 const { fetchMessageByIds, messageToEmbeds } = require("../util");
+
+const TRY_COUNT_THRESHOLD = 3;
 
 client.on(Events.MessageCreate, async message => {
   if (message.author.bot || message.channel.isVoiceBased()) return;
@@ -12,13 +15,19 @@ client.on(Events.MessageCreate, async message => {
       throw new Error('invalid url');
     }
 
-    const referredMessage = await fetchMessageByIds(guildId, channelId, messageId);
+    for (let tryCount = 0; tryCount < TRY_COUNT_THRESHOLD; ++tryCount) {
+      const referredMessage = await fetchMessageByIds(guildId, channelId, messageId);
 
-    if (referredMessage != null) {
-      const embeds = await messageToEmbeds(referredMessage);
+      if (referredMessage != null) {
+        const embeds = await messageToEmbeds(referredMessage);
 
-      if (embeds.length > 0) {
-        await message.channel.send({ embeds });
+        if (embeds.length > 0) {
+          await message.channel.send({ embeds });
+          break;
+        }
+      }
+      else {
+        log('discordUrlExpand:', 'fetches failed', tryCount + 1, guildId, channelId, messageId);
       }
     }
   }
