3c0de8571b7f251fc635d299b32e65e50ba75bf8
htsign <htsign@outlook.com>
2023-04-12 01:12:26 +0900

    add update command for weeklyAward

--- bot/features/weeklyAwards/commands.ts
+++ bot/features/weeklyAwards/commands.ts
@@ -20,12 +20,14 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
     async func(interaction: ChatInputCommandInteraction<'cached' | 'raw'>): Promise<void> {
       const guildId = interaction.guildId;
       const guildName = interaction.guild?.name;
-      const channel = interaction.options.getChannel('channel', true);
 
       if (guildName == null) {
         interaction.reply({ content: '登録したいサーバーの中で実行してください。', ephemeral: true });
         return;
       }
+
+      const channel = interaction.options.getChannel('channel', true);
+
       if (channel.type !== ChannelType.GuildAnnouncement && channel.type !== ChannelType.GuildText || 'permissions' in channel) {
         interaction.reply({ content: '適用できないチャンネルです。', ephemeral: true });
         return;
@@ -64,9 +66,57 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
       response.edit('リアクション大賞の巡回対象からこのサーバーを削除しました。');
     },
   },
+  update: {
+    description: '設定を変更します。',
+    options: [
+      {
+        name: 'channel',
+        description: '報告させたいチャンネル',
+        type: ApplicationCommandOptionType.Channel,
+        channelTypes: [ChannelType.GuildAnnouncement, ChannelType.GuildText],
+      }
+    ],
+    async func(interaction: ChatInputCommandInteraction<'cached' | 'raw'>): Promise<void> {
+      const { guildId, guild, user } = interaction;
+      const guildName = guild?.name;
+
+      if (guild == null || guildName == null) {
+        interaction.reply({ content: '登録したいサーバーの中で実行してください。', ephemeral: true });
+        return;
+      }
+
+      const configRecord = db.config.get(guildId);
+      if (configRecord == null) {
+        interaction.reply({ content: 'このサーバーはまだ登録されていません。', ephemeral: true });
+        return;
+      }
+
+      const channel = interaction.options.getChannel('channel') ?? await guild.channels.fetch(configRecord.channelId);
+
+      if (channel == null) {
+        interaction.reply({ content: '指定されたチャンネルは見つかりません。', ephemeral: true });
+        return;
+      }
+      if (channel.type !== ChannelType.GuildAnnouncement && channel.type !== ChannelType.GuildText || 'permissions' in channel) {
+        interaction.reply({ content: '適用できないチャンネルです。', ephemeral: true });
+        return;
+      }
+      else if (!interaction.guild?.members.me?.permissionsIn(channel).has(PermissionFlagsBits.SendMessages)) {
+        interaction.reply({ content: 'このチャンネルには発言する権限がありません。', ephemeral: true });
+        return;
+      }
+      log('register weeklyAward:', user.username, guildName);
+
+      const response = await interaction.deferReply();
+
+      await db.config.register(guildId, guildName, channel.id, channel.name);
+
+      response.edit(`週の報告を ${channel} で行うよう設定しました。`);
+    },
+  },
   status: {
     description: '現在のこのサーバーの登録状況を確認します。',
-    async func(interaction) {
+    async func(interaction: ChatInputCommandInteraction<'cached' | 'raw'>): Promise<void> {
       const { guildId, guild } = interaction;
       const guildName = guild?.name;
 
