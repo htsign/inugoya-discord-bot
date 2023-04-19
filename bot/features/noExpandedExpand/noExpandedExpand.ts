cb0d4cfa77cf11f031bc6e2ebba9368a96b1dbb6
htsign <htsign@outlook.com>
2023-04-19 20:43:59 +0900

    do not attach an author if `document.title` is unexpected format

--- bot/features/noExpandedExpand/noExpandedExpand.ts
+++ bot/features/noExpandedExpand/noExpandedExpand.ts
@@ -88,7 +88,7 @@ const getAuthor = async (document: Document, url: Url): ReturnType<typeof getAut
     const name = document.querySelector('meta[property="og:site_name"]')?.getAttribute('content');
     if (name != null) return [name, url];
 
-    const part = document.title.split(' - ').at(-1);
+    const part = document.title.includes(' - ') ? document.title.split(' - ').at(-1) : null;
     return part != null ? [part.trim(), url] : null;
   };
 
