32c540d2246c3d8704111aacc55e90d6e3241ad6
htsign <htsign@outlook.com>
2023-06-06 17:59:17 +0900

    add an option for earthquake

--- bot/features/earthquake/commands.ts
+++ bot/features/earthquake/commands.ts
@@ -11,7 +11,11 @@ import {
 import { log } from '@lib/log';
 import { DATETIME_FORMAT } from '@lib/util';
 import { db } from './db';
-import { UnexpectedIntensityError, intensityFromNumber, intensityFromNumberWithException } from './earthquake';
+import {
+  UnexpectedIntensityError,
+  intensityFromNumber,
+  intensityFromNumberWithException,
+} from './earthquake';
 import type { ChatInputCommandCollection } from 'types/bot';
 
 const DEFAULT_MIN_INTENSITY = 30;
@@ -29,7 +33,13 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
       },
       {
         name: 'minintensity',
-        description: `報告する最低の震度（デフォルト: ${DEFAULT_MIN_INTENSITY}）`,
+        description: `報告する最低の震度（デフォルト: ${intensityFromNumber(DEFAULT_MIN_INTENSITY)}）`,
+        type: ApplicationCommandOptionType.Integer,
+        choices: [10, 20, 30, 40, 45, 50, 55, 60, 70].map(n => ({ name: intensityFromNumber(n), value: n })),
+      },
+      {
+        name: 'alertthreshold',
+        description: `@here をつける最低の震度（デフォルト: ${intensityFromNumber(DEFAULT_MIN_INTENSITY)}）`,
         type: ApplicationCommandOptionType.Integer,
         choices: [10, 20, 30, 40, 45, 50, 55, 60, 70].map(n => ({ name: intensityFromNumber(n), value: n })),
       },
@@ -45,6 +55,7 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
 
       const channel = interaction.options.getChannel('channel', true);
       const minIntensity = interaction.options.getInteger('minintensity') ?? DEFAULT_MIN_INTENSITY;
+      const alertThreshold = interaction.options.getInteger('alertthreshold') ?? DEFAULT_MIN_INTENSITY;
 
       if (channel.type !== ChannelType.GuildAnnouncement && channel.type !== ChannelType.GuildText || 'permissions' in channel) {
         interaction.reply({ content: '適用できないチャンネルです。', ephemeral: true });
@@ -66,11 +77,23 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
           return;
         }
       }
+      try {
+        if (alertThreshold < 10 || 70 < alertThreshold) {
+          throw new UnexpectedIntensityError(alertThreshold);
+        }
+        intensityFromNumberWithException(alertThreshold);
+      }
+      catch (e) {
+        if (e instanceof UnexpectedIntensityError) {
+          interaction.reply({ content: `alertthreshold に不正の値が与えられました。 [${e.intensity}]`, ephemeral: true });
+          return;
+        }
+      }
       log('register earthquake:', interaction.user.username, guildName);
 
       const response = await interaction.deferReply();
 
-      await db.register(guildId, guildName, channel.id, channel.name, minIntensity),
+      await db.register(guildId, guildName, channel.id, channel.name, minIntensity, alertThreshold),
 
       response.edit(`${intensityFromNumber(minIntensity)}以上の地震速報をこのサーバーの ${channel} に通知するよう設定しました。`);
     },
@@ -109,6 +132,12 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
         type: ApplicationCommandOptionType.Integer,
         choices: [10, 20, 30, 40, 45, 50, 55, 60, 70].map(n => ({ name: intensityFromNumber(n), value: n })),
       },
+      {
+        name: 'alertthreshold',
+        description: '@here をつける最低の震度',
+        type: ApplicationCommandOptionType.Integer,
+        choices: [10, 20, 30, 40, 45, 50, 55, 60, 70].map(n => ({ name: intensityFromNumber(n), value: n })),
+      },
     ],
     async func(interaction: ChatInputCommandInteraction<'cached' | 'raw'>): Promise<void> {
       const { guildId, guild, user } = interaction;
@@ -127,10 +156,12 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
 
       const channel = interaction.options.getChannel('channel') ?? await guild.channels.fetch(configRecord.channelId);
       const minIntensity = interaction.options.getInteger('minintensity') ?? configRecord.minIntensity;
+      const alertThreshold = interaction.options.getInteger('alertthreshold') ?? configRecord.alertThreshold;
 
       const isSet = {
         channel: interaction.options.getChannel('channel') != null,
         minIntensity: interaction.options.getInteger('minintensity') != null,
+        alertThreshold: interaction.options.getInteger('alertthreshold') != null,
       };
 
       if (Object.values(isSet).every(x => !x)) {
@@ -161,11 +192,23 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
           return;
         }
       }
+      try {
+        if (alertThreshold < 10 || 70 < alertThreshold) {
+          throw new UnexpectedIntensityError(alertThreshold);
+        }
+        intensityFromNumberWithException(alertThreshold);
+      }
+      catch (e) {
+        if (e instanceof UnexpectedIntensityError) {
+          interaction.reply({ content: `alertthreshold に不正の値が与えられました。 [${e.intensity}]`, ephemeral: true });
+          return;
+        }
+      }
       log('register earthquake:', user.username, guildName);
 
       const response = await interaction.deferReply();
 
-      await db.register(guildId, guildName, channel.id, channel.name, minIntensity);
+      await db.register(guildId, guildName, channel.id, channel.name, minIntensity, alertThreshold);
 
       const fields: APIEmbedField[] = [];
 
@@ -174,6 +217,9 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
       }
       if (isSet.minIntensity) {
         fields.push({ name: '報告する最低の震度', value: intensityFromNumber(minIntensity) });
+      }
+      if (isSet.alertThreshold) {
+        fields.push({ name: '\\@here をつける最低の震度', value: intensityFromNumber(alertThreshold) });
       }
 
       response.edit({ content: '地震速報通知の設定を変更しました。', embeds: [{ fields }] });
@@ -197,12 +243,13 @@ const subCommands: ChatInputCommandCollection<void, {}, 'cached' | 'raw'> = {
       const embed = new EmbedBuilder({ title: '登録状況' });
 
       if (configRecord != null) {
-        const { channelId, minIntensity, createdAt, updatedAt } = configRecord;
+        const { channelId, minIntensity, alertThreshold, createdAt, updatedAt } = configRecord;
 
         embed.setDescription('登録済み').setColor(Colors.Green);
         embed.addFields(
           { name: '報告チャンネル', value: `<#${channelId}>`, inline: true },
           { name: '通知する最低の震度', value: intensityFromNumber(minIntensity), inline: true },
+          { name: '\\@here をつける最低の震度', value: intensityFromNumber(alertThreshold), inline: true },
           { name: ' ', value: '----------------' },
           { name: '初回設定日時', value: createdAt.format(DATETIME_FORMAT), inline: true },
           { name: '最終更新日時', value: updatedAt.format(DATETIME_FORMAT), inline: true },
--- bot/features/earthquake/db.ts
+++ bot/features/earthquake/db.ts
@@ -34,6 +34,7 @@ class EEWConfig {
         channelId: row.channel_id,
         channelName: row.channel_name,
         minIntensity: row.min_intensity,
+        alertThreshold: row.alert_threshold,
         createdAt: dayjs.utc(row.created_at).tz(),
         updatedAt: dayjs.utc(row.updated_at).tz(),
       }));
@@ -47,26 +48,36 @@ class EEWConfig {
         channel_id text not null,
         channel_name text not null,
         min_intensity integer not null,
+        alert_threshold integer not null,
         created_at text not null default (datetime('now')),
         updated_at text not null default (datetime('now'))
       )
     `).run();
   }
 
-  async register(guildId: string, guildName: string, channelId: string, channelName: string, minIntensity: number): Promise<void> {
+  async register(
+    guildId: string,
+    guildName: string,
+    channelId: string,
+    channelName: string,
+    minIntensity: number,
+    alertThreshold: number,
+  ): Promise<void> {
     const stmt = db.prepare(`
       insert into ${this.#TABLE} (
         guild_id,
         guild_name,
         channel_id,
         channel_name,
-        min_intensity
+        min_intensity,
+        alert_threshold
       ) values (
         $guildId,
         $guildName,
         $channelId,
         $channelName,
-        $minIntensity
+        $minIntensity,
+        $alertThreshold
       )
       on conflict (guild_id) do
         update set
@@ -74,6 +85,7 @@ class EEWConfig {
           channel_id = $channelId,
           channel_name = $channelName,
           min_intensity = $minIntensity,
+          alert_threshold = $alertThreshold
           updated_at = datetime('now')
     `);
 
@@ -84,12 +96,13 @@ class EEWConfig {
         $channelId: channelId,
         $channelName: channelName,
         $minIntensity: minIntensity,
+        $alertThreshold: alertThreshold,
       });
     }
     catch (e) {
       if (e instanceof TypeError && e.message.includes('database connection is busy')) {
         await setTimeout();
-        return this.register(guildId, guildName, channelId, channelName, minIntensity);
+        return this.register(guildId, guildName, channelId, channelName, minIntensity, alertThreshold);
       }
       throw e;
     }
@@ -131,6 +144,7 @@ class EEWConfig {
       channelId: row.channel_id,
       channelName: row.channel_name,
       minIntensity: row.min_intensity,
+      alertThreshold: row.alert_threshold,
       createdAt: dayjs.utc(row.created_at).tz(),
       updatedAt: dayjs.utc(row.updated_at).tz(),
     };
