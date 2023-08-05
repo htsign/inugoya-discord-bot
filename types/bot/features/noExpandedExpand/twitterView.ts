558099ee8d3d933d6dc562e39e33c89a0337c93b
htsign <htsign@outlook.com>
2023-08-05 17:32:14 +0900

    add a type information for 'extended_entities' property

--- types/bot/features/noExpandedExpand/twitterView.ts
+++ types/bot/features/noExpandedExpand/twitterView.ts
@@ -1,17 +1,32 @@
 export namespace TweetDetail {
   type PhotoSize = 'large' | 'medium' | 'small' | 'orig';
 
-  export interface Media {
+  export interface Media<MediaType extends string = 'photo'> {
     display_url: string;
     expanded_url: string;
     id_str: string;
     indices: [number, number];
     media_url_https: string;
-    type: 'photo';
+    type: MediaType;
     url: string;
     features: { [K in PhotoSize]: { faces: any[] } };
     sizes: { [K in PhotoSize]: { h: number, w: number, resize: string } };
     original_info: { height: number, width: number, focus_rects: Array<{ x: number, y: number, w: number, h: number }> };
+  }
+
+  export type ExtendedMedia = ExtendedPhoto | ExtendedVideo;
+
+  interface ExtendedMediaBase<MediaType extends string> extends Media<MediaType> {
+    media_key: string;
+    ext_media_availability: { status: string };
+  }
+
+  export interface ExtendedPhoto extends ExtendedMediaBase<'photo'> {}
+
+  export interface ExtendedVideo extends ExtendedMediaBase<'video'> {
+    additional_media_info: { monetizable: boolean };
+    mediaStats: { viewCount: number };
+    video_info: VideoInfo;
   }
 
   export interface Url {
@@ -20,6 +35,18 @@ export namespace TweetDetail {
     url: string;
     indices: [number, number];
   }
+
+  interface VideoInfo {
+    aspect_ratio: [number, number];
+    duration_millis: number;
+    variants: VideoVariant[];
+  }
+
+  interface VideoVariant {
+    bitrate?: number;
+    content_type: string;
+    url: string;
+  }
 }
 
 export interface TweetInfo {
@@ -27,6 +54,7 @@ export interface TweetInfo {
   userPic: string;
   tweet: string;
   pics: string[];
+  vids?: { url: string, key: string }[];
   timestamp: string;
   likes: number;
   retweets: number;
