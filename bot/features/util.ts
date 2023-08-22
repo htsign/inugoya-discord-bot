fa1ae7e086ba18eb0c13ab707dbe321082f62a6d
htsign <htsign@outlook.com>
2023-08-22 19:35:37 +0900

    minor refactor

--- bot/features/util.ts
+++ bot/features/util.ts
@@ -37,108 +37,105 @@ export const fetchMessageByIds = async (guildId: string, channelId: string, mess
 export const messageToEmbeds = async (message: Message<boolean>, options: EmbedMessageOptions[]): Promise<APIEmbed[]> => {
   const { channel } = message;
 
-  if (channel.isTextBased()) {
-    const optionsSet = new Set(options);
+  if (!channel.isTextBased()) return [];
 
-    const embeds: APIEmbed[] = [];
+  const optionsSet = new Set(options);
 
-    let author: User | null = null;
-    try {
-      author = await message.author.fetch();
-    }
-    catch (e) {
-      if (e instanceof Error) {
-        log(`${messageToEmbeds.name}:`, 'failed to fetch author', e.stack ?? `${e.name}: ${e.message}`);
-      }
-      else {
-        throw e;
-      }
-    }
+  const embeds: APIEmbed[] = [];
 
-    const getAverageColor = async (avatarUrl: Nullable<string>): Promise<number | null> => {
-      if (avatarUrl == null) return null;
-
-      const { value: [red, green, blue] } = await fastAvgColor.getAverageColor(avatarUrl, { silent: true });
-      return (red << 16) + (green << 8) + blue;
-    };
-
-    const embed = new EmbedBuilder()
-      .setURL(message.url)
-      .setTimestamp(message.editedTimestamp ?? message.createdTimestamp)
-      .setColor(author?.accentColor ?? await getAverageColor(author?.avatarURL()));
-
-    if (author != null) {
-      embed.setAuthor({ name: author.username, url: message.url, iconURL: author.displayAvatarURL() });
-    }
-
-    if (message.content !== '') {
-      embed.setDescription(message.content);
-    }
-
-    for (const option of optionsSet) {
-      switch (option) {
-        case 'reactions': {
-          const reactions = message.reactions.cache;
-          const reactionsCount = reactions.reduce((acc, x) => acc + x.count, 0);
-
-          if (reactionsCount > 0) {
-            embed.addFields({ name: 'Reactions', value: String(reactionsCount), inline: true });
-          }
-          break;
-        }
-        case 'originalLink': {
-          embed.addFields({ name: 'Original Link', value: message.url, inline: true });
-          break;
-        }
-        default: {
-          throw new RangeError(`exhaustive check: ${option satisfies never} is invalid`);
-        }
-      }
-    }
-
-    const [attachments, spoilerAttachments] = message.attachments.partition(x => !x.spoiler);
-
-    if (spoilerAttachments.size > 0) {
-      embed.addFields({ name: 'Spoilers', value: String(spoilerAttachments.size) });
-    }
-
-    if (!channel.isDMBased()) {
-      const text = channel.name;
-      const iconURL = message.guild?.iconURL();
-
-      if (iconURL != null) {
-        embed.setFooter({ text, iconURL });
-      }
-      else {
-        embed.setFooter({ text });
-      }
-    }
-
-    const getEmbedMediaType = (attachment: Attachment): 'image' | 'video' => {
-      const [type] = attachment.contentType?.split('/') ?? [];
-      return type === 'video' ? 'video' : 'image';
-    };
-
-    const attachmentArray = attachments.toJSON();
-    if (isNonEmpty(attachmentArray)) {
-      const attachment = attachmentArray[0];
-      const { url } = attachment;
-
-      embeds.push({ ...embed.toJSON(), [getEmbedMediaType(attachment)]: { url } });
+  let author: User | null = null;
+  try {
+    author = await message.author.fetch();
+  }
+  catch (e) {
+    if (e instanceof Error) {
+      log(`${messageToEmbeds.name}:`, 'failed to fetch author', e.stack ?? `${e.name}: ${e.message}`);
     }
     else {
-      embeds.push(embed.toJSON());
+      throw e;
     }
+  }
 
-    for (const attachment of attachmentArray.slice(1)) {
-      const { url } = attachment;
+  const getAverageColor = async (avatarUrl: Nullable<string>): Promise<number | null> => {
+    if (avatarUrl == null) return null;
 
-      embeds.push({ url: message.url, [getEmbedMediaType(attachment)]: { url } });
+    const { value: [red, green, blue] } = await fastAvgColor.getAverageColor(avatarUrl, { silent: true });
+    return (red << 16) + (green << 8) + blue;
+  };
+
+  const embed = new EmbedBuilder()
+    .setURL(message.url)
+    .setTimestamp(message.editedTimestamp ?? message.createdTimestamp)
+    .setColor(author?.accentColor ?? await getAverageColor(author?.avatarURL()));
+
+  if (author != null) {
+    embed.setAuthor({ name: author.username, url: message.url, iconURL: author.displayAvatarURL() });
+  }
+
+  if (message.content !== '') {
+    embed.setDescription(message.content);
+  }
+
+  for (const option of optionsSet) {
+    switch (option) {
+      case 'reactions': {
+        const reactions = message.reactions.cache;
+        const reactionsCount = reactions.reduce((acc, x) => acc + x.count, 0);
+
+        if (reactionsCount > 0) {
+          embed.addFields({ name: 'Reactions', value: String(reactionsCount), inline: true });
+        }
+        break;
+      }
+      case 'originalLink': {
+        embed.addFields({ name: 'Original Link', value: message.url, inline: true });
+        break;
+      }
+      default: {
+        throw new RangeError(`exhaustive check: ${option satisfies never} is invalid`);
+      }
     }
+  }
 
-    return embeds;
+  const [attachments, spoilerAttachments] = message.attachments.partition(x => !x.spoiler);
+
+  if (spoilerAttachments.size > 0) {
+    embed.addFields({ name: 'Spoilers', value: String(spoilerAttachments.size) });
+  }
+
+  if (!channel.isDMBased()) {
+    const text = channel.name;
+    const iconURL = message.guild?.iconURL();
+
+    if (iconURL != null) {
+      embed.setFooter({ text, iconURL });
+    }
+    else {
+      embed.setFooter({ text });
+    }
+  }
+
+  const getEmbedMediaType = (attachment: Attachment): 'image' | 'video' => {
+    const [type] = attachment.contentType?.split('/') ?? [];
+    return type === 'video' ? 'video' : 'image';
+  };
+
+  const attachmentArray = attachments.toJSON();
+  if (isNonEmpty(attachmentArray)) {
+    const attachment = attachmentArray[0];
+    const { url } = attachment;
+
+    embeds.push({ ...embed.toJSON(), [getEmbedMediaType(attachment)]: { url } });
   }
   else {
-    return [];
+    embeds.push(embed.toJSON());
   }
+
+  for (const attachment of attachmentArray.slice(1)) {
+    const { url } = attachment;
+
+    embeds.push({ url: message.url, [getEmbedMediaType(attachment)]: { url } });
+  }
+
+  return embeds;
 };
