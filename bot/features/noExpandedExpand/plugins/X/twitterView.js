df591fe3bb24bba71d53861c4e8eb1a886ddf242
htsign <htsign@outlook.com>
2023-10-07 17:52:33 +0900

    minor fix

--- bot/features/noExpandedExpand/plugins/X/twitterView.js
+++ bot/features/noExpandedExpand/plugins/X/twitterView.js
@@ -152,7 +152,7 @@ export const hooks = [
           const apiUrl = response.url();
 
           if (apiUrl.startsWith('https://twitter.com/i/api/') && apiUrl.includes('TweetDetail?')) {
-            const [, statusId] = url.match(/\/status\/(\d+?)$/) ?? [];
+            const [, statusId] = url.match(/\/status\/(\d+?)\D/) ?? [];
             if (statusId == null) {
               log('twitterView:', 'failed to get tweet details', 'no statusId');
               return;
