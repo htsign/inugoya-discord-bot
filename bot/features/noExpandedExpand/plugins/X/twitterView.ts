126aa0a2562dca702710537afe0d0160d6ac68c1
htsign <htsign@outlook.com>
2023-10-11 20:42:23 +0900

    change regexp string for extracting statusId

--- bot/features/noExpandedExpand/plugins/X/twitterView.ts
+++ bot/features/noExpandedExpand/plugins/X/twitterView.ts
@@ -147,7 +147,7 @@ export const hooks: PluginHooks = [
           const apiUrl = response.url();
 
           if (apiUrl.startsWith('https://twitter.com/i/api/') && apiUrl.includes('TweetDetail?')) {
-            const [, statusId] = url.match(/\/status\/(\d+?)\D/) ?? [];
+            const [, statusId] = url.match(/\/status\/([0-9]+?)\b/) ?? [];
             if (statusId == null) {
               log('twitterView:', 'failed to get tweet details', 'no statusId');
               return;
