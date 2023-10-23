885ff4aee92eb1f6dc85416f700031c822d6e231
htsign <htsign@outlook.com>
2023-10-23 14:43:36 +0900

    cut a double pipe tailed in url

--- types/bot/features/noExpandedExpand/index.ts
+++ types/bot/features/noExpandedExpand/index.ts
@@ -1,12 +1,11 @@
 import type { APIEmbed, AttachmentBuilder, Awaitable, ClientEvents } from 'discord.js';
-import { Url } from 'types';
 
 export interface Plugin {
   handlers?: PluginHandlers;
   hooks?: PluginHooks;
 }
 export type PluginHandlers = Partial<{ [K in keyof ClientEvents]: (...args: ClientEvents[K]) => Awaitable<void> }>;
-export type PluginHooks = [string | RegExp, (url: Url, index: number) => Promise<HookResult>][];
+export type PluginHooks = [string | RegExp, (url: string, index: number) => Promise<HookResult>][];
 export interface HookResult {
   embeds: APIEmbed[];
   attachments: AttachmentBuilder[];
