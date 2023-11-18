78b00420e53fc591030ef3c6acda0a9ca64c8093
htsign <htsign@outlook.com>
2023-11-18 14:44:23 +0900

    export useragent

--- bot/lib/util.js
+++ bot/lib/util.js
@@ -6,6 +6,8 @@ import { JSDOM } from 'jsdom';
 
 export const URL_REGEX_GLOBAL = /\bhttps?:\/\/\S+/g;
 export const DATETIME_FORMAT = 'YYYY/MM/DD HH:mm:ss';
+export const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0';
+
 const configOutput = dotenv.config();
 
 /**
@@ -85,7 +87,7 @@ export const urlToDocument = async url => {
     'Accept-Language': 'ja,en-US;en;q=0.3',
     'Cache-Control': 'no-cache',
     'Pragma': 'no-cache',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
+    'User-Agent': USER_AGENT,
   };
 
   /**
